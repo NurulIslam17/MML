d5cef9455552312dd7c8859db1700f26e5d90d17
@Nurul97 <nurulislamnextgenitltd.com>
2023-08-06 16:05:39 +0600

    Deposite UI

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bazar;
+use App\Models\Meal;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,7 @@ class DashboardController extends Controller
     {
         $total_cost = Bazar::sum('price');
         $total_users = User::count();
-        return view('backend.dashboard.index',compact('total_cost','total_users'));
+        $totla_meals = Meal::sum('meal_no');
+        return view('backend.dashboard.index',compact('total_cost','total_users','totla_meals'));
     }
 }
---
+++ app/Http/Controllers/Backend/DepositeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DepositeController extends Controller
+{
+    public function index()
+    {
+        return view('backend.deposite.index');
+    }
+    public function create()
+    {
+        $users = User::latest()->get();
+        return view('backend.deposite.create',compact('users'));
+    }
+}
